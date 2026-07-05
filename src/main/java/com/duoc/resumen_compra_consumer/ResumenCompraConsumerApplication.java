1b77858d98915c155395d4161569a37aa1c864b6
m-vargas-h <m.vargas@outlook.cl>
2026-07-04 22:53:44 -0400

    Initial commit

---
+++ src/main/java/com/duoc/resumen_compra_consumer/ResumenCompraConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.resumen_compra_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumenCompraConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumenCompraConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/resumen_compra_consumer/ResumenCompraConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.resumen_compra_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumenCompraConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
